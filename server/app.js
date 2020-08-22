358d269ea949dc184fb596ed10c2f449a1bc1da5
theosakram <wyrdhn@gmail.com>
2020-08-22 11:46:26 +0700

    turn and movement on clint with socket

--- server/app.js
+++ server/app.js
@@ -3,35 +3,67 @@ const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 const port = process.env.PORT || 3000;
 const cors = require("cors");
-const Game = require("./logic/Turns");
-const Player = require("./logic/Player");
+const {
+  Game,
+  Player,
+  Market,
+  LuxuryShop,
+  PoliceOffice,
+  TeaHouse,
+  WainWright,
+  Warehouse,
+} = require("./game_data");
 app.use(cors());
 
 let rooms = [],
+  playersToBe = [],
   players = [],
-  users = [];
+  tiles = [
+    new Market(false),
+    new LuxuryShop(false),
+    new PoliceOffice(false),
+    new TeaHouse(false),
+    new WainWright(false),
+    new Warehouse(false),
+  ];
+users = [];
+
+let objGame = {
+  players: [],
+  active: "",
+  currentLocation: "",
+};
+
+const g = new Game();
 
 io.on("connection", (socket) => {
   console.log("User connected: " + socket.id);
-  if (users.some(user => user.id === socket.id)) console.log('sudah ada username')
+  if (users.some((user) => user.id === socket.id))
+    console.log("sudah ada username");
 
   socket.on("submit-username", (name) => {
-    if (users.some(user => user.id === socket.id)) {
-      console.log('Splice user dengan id yang sama (Handle double-data)')
-      users.splice(users.findIndex(user => user.id === socket.id), 1)
+    if (users.some((user) => user.id === socket.id)) {
+      console.log("Splice user dengan id yang sama (Handle double-data)");
+      users.splice(
+        users.findIndex((user) => user.id === socket.id),
+        1
+      );
     }
     const user = {
       name,
       id: socket.id,
     };
     users.push(user);
-    console.log(users)
+    console.log(users);
     socket.emit("get-username", user);
   });
   socket.on("logout", () => {
-    if (users.some(user => user.id === socket.id)) {
-      console.log('Splice user dengan id yang disconnect (Handle logout)')
-      users.splice(users.findIndex(user => user.id === socket.id), 1)
+    if (users.some((user) => user.id === socket.id)) {
+      console.log("Splice user dengan id yang disconnect (Handle logout)");
+      users.splice(
+        users.findIndex((user) => user.id === socket.id),
+        1
+      );
     }
     rooms = [];
   });
@@ -56,38 +88,71 @@ io.on("connection", (socket) => {
         io.emit("updated-room", rooms);
       } else {
         rooms[index].users.push(data.username);
+        playersToBe.push(data.username);
         io.sockets.in(data.roomName).emit("room-detail", rooms[index]);
       }
     });
   });
 
   socket.on("start-game", (data) => {
-    // console.log(data);
-    const game = new Game();
-    const p1 = new Player(data.users[0]);
-    const p2 = new Player(data.users[1]);
-    game.assign(p1);
-    game.assign(p2);
-    game.setPlays();
-    game.setGolds();
-    game.initialize();
-    io.in(data.name).emit("start-game", data, game);
+    const p1 = new Player(playersToBe[0].name);
+    const p2 = new Player(playersToBe[1].name);
+    g.assign(p1);
+    g.assign(p2);
+    g.setPlays();
+    g.setGolds();
+    g.initialize();
+
+    objGame.players = [g.players[0], g.players[1]];
+    objGame.active = g.activeCharacter;
+
+    players.push(p1, p2);
+
+    io.in(data.name).emit("start-game", data, objGame, tiles);
     // io.to(data.name).emit("start-game", data);
   });
-  socket.on("updated-data", (data, game) => {
-    console.log(data);
-    console.log(game);
-    game.players[0].gold += 1;
-    io.in(data).emit("updated-game", game);
+  socket.on("updated-data", (data) => {
+    player = players.filter((x) => x.name === g.activeCharacter);
+    player[0].gold += 1;
+    player[0].hasDone += 1;
+
+    if (player[0].hasDone === 2) {
+      g.checkTurn();
+      player[0].hasDone = 0;
+    }
+
+    objGame.players = [players[0], g.players[1]];
+    objGame.active = g.activeCharacter;
+
+    io.in(data).emit("updated-game", objGame);
   });
 
-  socket.on('disconnect', () => {
-    if (users.some(user => user.id === socket.id)) {
-      console.log('Splice user dengan id yang disconnect (Handle disconnect)')
-      users.splice(users.findIndex(user => user.id === socket.id), 1)
+  socket.on("move", (data, moveFrom, moveTo) => {
+    player = players.filter((x) => x.name === g.activeCharacter);
+    player[0].move((moveFrom = ""), moveTo);
+
+    if (player[0].hasDone === 2) {
+      g.checkTurn();
+      player[0].hasDone = 0;
     }
-    console.log((`User ${socket.id} disconnected.`))
-  })
+
+    objGame.players = [players[0], g.players[1]];
+    objGame.active = g.activeCharacter;
+    objGame.currentLocation = player[0].currentLocation;
+
+    io.in(data).emit("move-game", objGame);
+  });
+
+  socket.on("disconnect", () => {
+    if (users.some((user) => user.id === socket.id)) {
+      console.log("Splice user dengan id yang disconnect (Handle disconnect)");
+      users.splice(
+        users.findIndex((user) => user.id === socket.id),
+        1
+      );
+    }
+    console.log(`User ${socket.id} disconnected.`);
+  });
 });
 
 server.listen(port, () => console.log(`Running on port ${port}`));
