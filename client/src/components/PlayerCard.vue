3c63d7bd2bf5f34c3b62e792d1323ebe6f978bfd
NickoValentino <nicko.valentinoo@gmail.com>
2020-08-25 13:24:51 +0800

    Styling player card and import new assets

--- client/src/components/PlayerCard.vue
+++ client/src/components/PlayerCard.vue
@@ -1,73 +1,112 @@
 <template>
   <div class="card bg-gray-500 bg-opacity-75 border-2 p-2 flex">
-    <div id="kiri" class="w-4/6 border">
-      <div class="border-b-2">
-        <p>
+    <!-- KIRI -->
+    <div id="kiri" class="w-4/6">
+      <div class="border-b-2 bg-blue-900">
+        <p class="text-gray-100 text-2xl">
           <i class="fas fa-chess-king"></i>
           {{player.name}}
         </p>
       </div>
-      <div>
-        <div class="flex justify-evenly">
-          <div>
-            <p>
-              <i class="fas fa-drumstick-bite text-red-600"></i> {{player.resources[0].amount}}
-            </p>
+
+      <div class="flex flex-col">
+        <div class="flex flex-col">
+          <div class="flex">
+            <img
+              v-for="(n,i) in player.resources[0].amount"
+              :src="require(`../assets/apple.png`)"
+              :key="i"
+              width="24"
+              height="24"
+            />
           </div>
-          <div>
-            <p>
-              <i class="fa fa-level-up" aria-hidden="true"></i>{{player.cart}}
-            </p>
+          <div class="flex">
+            <img
+              v-for="(n,i) in player.resources[1].amount"
+              :src="require(`../assets/clothes.png`)"
+              :key="i"
+              width="24"
+              height="24"
+            />
+          </div>
+          <div class="flex">
+            <img
+              v-for="(n,i) in player.resources[2].amount"
+              :src="require(`../assets/jewelry.png`)"
+              :key="i"
+              width="24"
+              height="24"
+            />
           </div>
         </div>
-        <div class="flex justify-evenly">
-          <div>
-            <p>
-              <i class="fas fa-tshirt text-green-600"></i> {{player.resources[1].amount}}
-            </p>
+
+        <div class="flex flex-col">
+          <div class="flex">
+            <i class="fa fa-level-up" aria-hidden="true"></i>
+            {{player.cart}}
           </div>
-          <div>
-            <p>
-              <i class="fas fa-boxes"></i> {{player.capacity}}
-            </p>
+          <div class="flex">
+            <i class="fas fa-boxes"></i>
+            {{player.capacity}}
           </div>
-        </div>
-        <div class="flex justify-evenly">
-          <div>
-            <p>
-              <i class="fas fa-ring text-blue-600"></i> {{player.resources[2].amount}}
-            </p>
-          </div>
-          <div>
-            <p>
-              <i class="fas fa-hands-helping"></i> {{playerAssis}}
-            </p>
+          <div class="flex">
+            <img
+              v-for="(n,i) in playerAssis"
+              :src="require(`../assets/assistant.png`)"
+              :key="i"
+              width="24"
+              height="24"
+            />
           </div>
         </div>
       </div>
     </div>
+    <!-- END KIRI -->
+
+    <!-- START KANAN -->
     <div id="kanan" class="w-2/6 border ml-2">
-      <div>
-        <p>avatar</p>
-      </div>
-      <div>
-        <p>
-          <i class="fas fa-gem text-red-400"></i> {{player.diamond}}
-        </p>
-      </div>
-      <div>
-        <p>
-          <i class="fas fa-coins text-yellow-500"></i>
-          {{player.gold}}
-        </p>
-      </div>
-      <div>
-        <p>
-          <i class="fas fa-shoe-prints"></i>
-          {{player.movement}}
-        </p>
+      <div class="flex relative">
+        <!-- AVATAR -->
+        <img
+          class="absolute top-0 right-0 -mt-5 -mr-5 rounded-full border-2"
+          src="https://dummyimage.com/64.jpg"
+          alt
+          width="64px"
+          height="64px"
+          srcset
+        />
+
+        <!-- FLEX DIAMOND -->
+        <div class="w-1/2 flex flex-col">
+          <img
+            v-for="(n,i) in 6"
+            :class="[player.diamond<=i ? 'opacity-50' : 'opacity-100']"
+            :src="require(`../assets/diamond.png`)"
+            :key="i"
+            width="24"
+            height="24"
+          />
+        </div>
+
+        <!-- FLEX GOLD + MVSPEED -->
+        <div class="w-1/2 flex flex-col justify-end font-bold">
+          <!-- GOLD -->
+          <div class="flex">
+            <img :src="require(`../assets/gold.png`)" :key="i" width="24" height="24" />
+            <p>{{player.gold}}</p>
+          </div>
+          <!-- MVSPEED -->
+          <div class="flex">
+            <p>
+              <i class="fas fa-shoe-prints"></i>
+              {{player.movement}}
+            </p>
+          </div>
+        </div>
+        <!-- END GOLD + MVSPEED -->
       </div>
     </div>
+    <!-- END KANAN -->
   </div>
 </template>
 
